ec2c6051ca1f33b0c2bb862bd9a5bebfcc742099
alejandrocalderonhernandez <alejandro950825@gmail.com>
2023-07-25 17:56:26 -0600

    add configs

--- src/main/java/com/debuggeandoideas/app_security/security/SecurityConfig.java
+++ src/main/java/com/debuggeandoideas/app_security/security/SecurityConfig.java
@@ -3,58 +3,61 @@ package com.debuggeandoideas.app_security.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsPasswordService;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import javax.sql.DataSource;
+import java.util.List;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        var requestHandler = new CsrfTokenRequestAttributeHandler();
+        requestHandler.setCsrfRequestAttributeName("_csrf");
         http.authorizeHttpRequests(auth ->
-                auth.requestMatchers("/loans", "/balance", "/accounts", "/cards")
-                        .authenticated()
-                        .anyRequest().permitAll())
+                        auth.requestMatchers("/loans", "/balance", "/accounts", "/cards")
+                                .authenticated()
+                                .anyRequest().permitAll())
                 .formLogin(Customizer.withDefaults())
                 .httpBasic(Customizer.withDefaults());
+        http.cors(cors -> corsConfigurationSource());
+        http.csrf(csrf -> csrf
+                        .csrfTokenRequestHandler(requestHandler)
+                        .ignoringRequestMatchers("/welcome", "/about_us")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class);
         return http.build();
     }
 
-    /*@Bean
-    InMemoryUserDetailsManager inMemoryUserDetailsManager() {
-        var admin = User.withUsername("admin")
-                .password("to_be_encoded")
-                .authorities("ADMIN")
-                .build();
-
-        var user = User.withUsername("user")
-                .password("to_be_encoded")
-                .authorities("USER")
-                .build();
-
-        return new InMemoryUserDetailsManager(admin, user);
-    }*/
-
-    /*@Bean
-    UserDetailsService userDetailsService(DataSource dataSource) {
-        return new JdbcUserDetailsManager(dataSource);
-    }*/
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
 
     @Bean
-    PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    CorsConfigurationSource corsConfigurationSource() {
+        var config = new CorsConfiguration();
+
+        //config.setAllowedOrigins(List.of("http://localhost:4200", "http://my-app.com"));
+        config.setAllowedOrigins(List.of("*"));
+        //config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
+        config.setAllowedMethods(List.of("*"));
+        config.setAllowedHeaders(List.of("*"));
+
+        var source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+
+        return source;
     }
+
 }
